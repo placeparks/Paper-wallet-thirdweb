3ec3b1aa6a26b37e2661303d9c9394977cd20547
placeparks <86215469+placeparks@users.noreply.github.com>
2023-07-21 16:46:06 +0500

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,65 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ConnectWallet, useAddress } from '@thirdweb-dev/react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 function Navbar() {
   const address = useAddress();
+  const [isOpen, setIsOpen] = useState(false); // Add this state for mobile menu
 
   return (
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar
-          </a>
+          <Link href="/">
+            <div className="navbar-brand">Navbar</div>
+          </Link>
           <button
+            onClick={() => setIsOpen(!isOpen)} // Toggle menu on click
             className="navbar-toggler"
             type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="navbarSupportedContent"> {/* Show menu when isOpen is true */}
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
-                  Home
-                </a>
+                <Link href="/">
+                  <div className="nav-link active" aria-current="page">Home</div>
+                </Link>
               </li>
-            
-             
-              {!address ? (
-                <ConnectWallet btnTitle="Login" />
-              ) : (
-                <Link href={`/profile/${address}`}>
-                  <img
+            </ul>
+   
+            {!address ? (
+              <ConnectWallet btnTitle="Login" className="ml-auto" /> // Add "ml-auto" to push button to the right
+            ) : (
+              <Link href={`/profile/${address}`}>
+                <div>
+                  <Image
                     src="https://avatars.githubusercontent.com/u/81866624?v=4"
                     alt="profile"
-                    style={{
-                      width: '40px',  // Adjust the width as needed
-                      height: '40px', // Adjust the height as needed
-                      borderRadius: '50%',
-                    }}
+                    width={40}
+                    height={40}
                   />
-                </Link>
-              )}
-            </ul>
-            <form className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
+                </div>
+              </Link>
+            )}
           </div>
         </div>
       </nav>
